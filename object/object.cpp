168c976e92263d869ff8c98cad1b875fdf943b50
paul <13136918273@gmail.com>
2024-10-02 10:27:56 +0800

    等待求值

--- object/object.cpp
+++ object/object.cpp
@@ -1,1 +1,28 @@
+#include <ast/node.h>
+#include <cstdarg>
 #include <object/object.h>
+#include <string.h>
+using namespace lisp::object;
+using namespace std;
+unordered_map<Object::Type,string> Object::names={
+{OBJECT_ERROR,"error"},
+{OBJECT_INTEGER,"integer"}}
+;
+string Object::get_name(Type type) const {
+    auto it=names.find(type);
+    if(it!=names.end()) {
+        return it->second;
+    }
+    return "";
+}
+ std::shared_ptr<Object> Object::new_error(const char *format,...) {
+     char buf[1024]={{0}};
+    va_list arg_ptr;
+    va_start(arg_ptr,format);
+    vsnprintf(buf,sizeof(buf),format,arg_ptr);
+    va_end(arg_ptr);
+   return std::shared_ptr<Error> (new Error(buf));
+};
+ std::shared_ptr<Object>  Object::new_integer(int val) {
+return shared_ptr<Integer>(new Integer(val));
+};
--- object/object.h
+++ object/object.h
@@ -1,7 +1,7 @@
-
-#pragma once;
-#include <unordered_map>
+#pragma once
+#include <memory>
 #include <string>
+#include <unordered_map>
 using namespace std;
 namespace lisp {
     namespace object {
@@ -12,11 +12,34 @@ namespace lisp {
             Object(Type type):type(type){}
             virtual ~Object(){}
             Type get_type() const {return type;}
-            string get_name() const;
+            string get_name(Type type) const;
             virtual string str() const=0;
+            //报错信息
+            static std::shared_ptr<Object> new_error(const char *format,...);
+            static std::shared_ptr<Object>  new_integer(int val);
         private :
             Type type;
             static unordered_map<Type,string> names;
+        };
+        class Integer :public Object {
+        public: Integer():Object(OBJECT_INTEGER),val(0){}
+            Integer(int val):Object(OBJECT_INTEGER),val(val){}
+            virtual ~Integer(){}
+            string str()const {
+             return std::to_string(val);
+        } ;
+        public:
+            int val;
+        };
+        class Error :public Object {
+        public: Error():Object(OBJECT_ERROR){}
+            Error(string message):Object(OBJECT_ERROR),message(message){}
+            virtual ~Error(){}
+            string str()const {
+            return "error:"+message;
+        } ;
+        public:
+            string message;
         };
     } // namespace object
 } // namespace lisp
