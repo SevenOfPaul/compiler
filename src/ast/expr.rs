41eb4e6b1a43748c8d57a7ec03b066e9c53f4308
paul <83870824+SevenOfPaul@users.noreply.github.com>
2024-10-23 16:58:42 +0800

    提交代码

--- src/ast/expr.rs
+++ src/ast/expr.rs
@@ -1,29 +1,45 @@
-use crate::ast::token::object::Object;
-use crate::ast::token::token_type::Token_type;
-use std::fmt::Binary;
-pub(crate) enum Expr {
-    Binary {
-        operator: Token_type,
-        l_expression: Box<Expr>,
-        r_expression: Box<Expr>,
-    },
-    Grouping {
-        expression: Box<Expr>,
-    },
-    Literal {
-        val: Option<Object>,
-    },
-    Unary {
-        operator: Token_type,
-        r_expression: Box<Expr>,
-    },
+use crate::token::token_type::Token_type;
+// abstract class Expr {
+// interface Visitor<R> {
+// R visitAssignExpr(Assign expr);
+// R visitBinaryExpr(Binary expr);
+// R visitCallExpr(Call expr);
+// R visitGetExpr(Get expr);
+// R visitGroupingExpr(Grouping expr);
+// R visitLiteralExpr(Literal expr);
+// R visitLogicalExpr(Logical expr);
+// R visitSetExpr(Set expr);
+// R visitSuperExpr(Super expr);
+// R visitThisExpr(This expr);
+// R visitUnaryExpr(Unary expr);
+// R visitVariableExpr(Variable expr);
+// }
+pub (crate) enum Expr{
+    AssignExpr(Assign),
+    BinaryExpr(Binary),
 }
-
+struct Assign{
+    name:Token_type,val:Box<Expr>
+}
+struct Binary{
+    name:Token_type,left:Box<Expr>,right:Box<Expr>
+}
+impl Assign{
+    fn new(name:Token_type, val:Box<Expr>) -> Assign{
+        Self{name,val}
+    }
+}
+impl Binary{
+    fn new(name:Token_type, left:Box<Expr>,right:Box<Expr>) -> Self{
+       Self {name,left,right}
+    }
+}
 pub trait Visitor<T> {
-    fn visit(&self, expr: &Expr) -> T;
+   fn visit_assign(&self,expr:&Assign) -> T;
+    fn visit_binary(&self,expr:&Binary) -> T;
 }
-impl Expr {
-    pub fn accept<T>(&self, visitor: &mut dyn Visitor<T>) -> T {
-        visitor.visit(&self)
+impl Visitor<Expr> for Vec<Expr>{
+    fn visit_assign(&self,expr:Assign) -> Expr{
+
     }
 }
