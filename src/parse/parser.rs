3062e9bff14ef3d494012bab84bd88e8c204f852
paul <13136918273@gmail.com>
2024-10-28 21:58:51 +0800

    提交equality函数

--- src/parse/parser.rs
+++ src/parse/parser.rs
@@ -10,34 +10,46 @@ impl Parser{
     fn new(tokens:Vec<Token>)->Self{
         Self{tokens,pos:0}
     }
-    fn expression(&self)->Expr{
+    fn expression(&mut self) ->Expr{
         self.equality()
     }
-    fn equality(&self)->Expr{
+    fn equality(&mut self) ->Expr{
+        let mut  expr=   self.comparison();
+        while self.fulfill(vec![Token_type::BANG_EQUAL,Token_type::EQUAL_EQUAL]) {
+            let operator = self.previous();
+            let r = self.comparison();
+            expr = Expr::Binary {
+                l_expression: Box::from(expr),
+                operator: operator.clone(),
+                r_expression: Box::from(r)
+            }
+        }
+        expr
+    }
+    fn comparison(&self)->Expr{
 
     }
     fn advance(&mut self)->&Token{
         if !self.is_end(){
             self.pos += 1;
         }
-         self.previous()
+        self.previous()
     }
     fn check(&self,token_type:Token_type)->bool{
         if self.is_end(){
-              false
+            false
         }else {
             (*self.peek()).token_type == token_type
         }
     }
-
     fn fulfill(&mut self, types:Vec<Token_type>) ->bool{
         for t in types{
-         if  self.check(t){
-             self.advance();
-            return true;
-         }
+            if  self.check(t){
+                self.advance();
+                return true;
+            }
         }
-          false
+        false
     }
     fn is_end(&self)->bool{
         (*self.previous()).token_type==Token_type::EOF
