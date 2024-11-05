a5d5e733f7744bf452f3e2b18ab79068f208e194
paul <13136918273@gmail.com>
2024-11-05 11:18:53 +0800

    提交大改

--- src/interpret/interpreter.rs
+++ src/interpret/interpreter.rs
@@ -1,4 +1,5 @@
 use crate::ast::expr::{Expr, Visitor};
+use crate::ast::token::object::Object;
 use crate::ast::token::token::Token;
 use crate::ast::token::token_type::Token_Type::NIL;
 use crate::interpret::error::Run_Err;
@@ -6,15 +7,20 @@ use crate::interpret::value::Value;
 
 struct Interpreter {}
 impl Visitor<Result<Value,Run_Err>> for Interpreter {
-    fn visit(&self, expr: &Expr) -> Result<Value,Run_Err> {
-     match expr {
-         Expr::Literal {val}=>{
-             // Ok(Value::)
-         },
-             _=>{
-                 Err(Run_Err::new(Token::new(NIL,"")))
-             }
-     }
+    fn visit_binary(&mut self, operator: &Token, l_expression: &Expr, r_expression: &Expr) -> Result<Value, Run_Err> {
+        todo!()
+    }
+
+    fn visit_grouping(&mut self, expression: &Expr) -> Result<Value, Run_Err> {
+        todo!()
+    }
+
+    fn visit_literal(&mut self, value: &Option<Object>) -> Result<Value, Run_Err> {
+        todo!()
+    }
+
+    fn visit_unary(&mut self, operator: &Token, r_expression: &Expr) -> Result<Value, Run_Err> {
+        todo!()
     }
 }
 impl Interpreter {
