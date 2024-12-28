2160e7e3dba4f043e7cd70595d8d0dc91c381355
paul <13136918273@gmail.com>
2024-12-28 15:53:10 +0800

    在ai的帮助下 将运算结果输出到div上

--- src/error/report.rs
+++ src/error/report.rs
@@ -1,10 +1,14 @@
-use color_string::Font::*;
-use color_string::{cs, fonts};
-pub fn log(line :usize,pos:&str, message:&str){
- report(line,pos,message);
+#[cfg(target_arch = "wasm32")]
+pub fn log(line: usize, pos: &str, message: &str) {
+    use crate::tools::printf;
+    printf(format!("错误: 在行{}位置{}发生了'{}'这个错误！", line, pos, message));
 }
-fn report(line :usize,pos:&str,message:&str){
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn log(line: usize, pos: &str, message: &str) {
+    use color_string::Font::*;
+    use color_string::{cs, fonts};
     let fonts = fonts!(Red);
-    println!("{}",cs!(fonts=>"请检查你的代码"));
-    println!("{}", cs!(fonts =>format!("错误:在行{}位置{}发生了'{}'这个错误！ ",line,pos,message)))
+    println!("{}", cs!(fonts=>"请检查你的代码"));
+    println!("{}", cs!(fonts=>format!("错误:在行{}位置{}发生了'{}'这个错误！ ",line,pos,message)))
 }
--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,12 +1,29 @@
 use crate::interpret::value::Value;
-
-pub(crate) fn printf(v:Value){
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+pub(crate) fn println(v:Value){
     match v {
-        Value::Num(num) => println!("{}", num),
-        Value::Str(s)=>println!("{}", s),
-        Value::Bool(b)=>println!("{}", b),
-        Value::Time(t)=>println!("{:?}", t),
-         Value::Func(f)=>println!("{:?}", f.to_string()),
-        _=>println!("{:?}",Value::Nil)
+        Value::Num(num) => printf( num),
+        Value::Str(s)=>printf( s),
+        Value::Bool(b)=>printf( b),
+        Value::Time(t)=>printf(t),
+         Value::Func(f)=>printf(f.to_string()),
+        _=>printf(Value::Nil)
     }
+}
+
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen]
+extern "C" {
+    fn append_to_output(s: &str, is_error: bool);
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn append_to_output(s: &str, is_error: bool) {
+    println!("{}", s);
+}
+
+pub fn printf<T: std::fmt::Display>(val: T) {
+    append_to_output(&format!("{}", val), false);
 }
